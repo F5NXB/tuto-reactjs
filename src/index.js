13c661de6081d1732ce356a44b99d5156a3f8f80
F5NXB <homehera@gmail.com>
2019-09-11 06:56:44 +0200

    gestion des événements
    Comment passer un gestionnaire d’événements (par exemple onClick) à un composant ?

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
-
 class App extends React.Component {
+  handleClick_1 = () => {
+    console.log("Click_1");
+  };
+
+  handleClick_2() {
+    console.log("Click_2");
+  }
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="container">
+        <p>Première méthode</p>
+        <button onClick={this.handleClick_1}>Cliquez ici</button>
+        <p>Deuxième méthode</p>
+        <button onClick={this.handleClick_2.bind(this)}>Cliquez ici</button>
+      </div>
+    );
   }
 }
 
