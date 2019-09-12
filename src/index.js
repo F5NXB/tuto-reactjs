ef948db493a3d864d2c855addf355889bd9d7834
F5NXB <homehera@gmail.com>
2019-09-12 07:49:21 +0200

    Gérer un formulaire - createRef
    Récupérer la valeur d'un input après
    soumission

--- src/index.js
+++ src/index.js
@@ -3,50 +3,30 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 class App extends React.Component {
+  inputNom = React.createRef();
+
   state = {
     id: 1
   };
 
-  handleClick_1 = () => {
-    console.log("Click_1");
+  handleSubmit = event => {
+    event.preventDefault(); // évite le rechargement de la page
+    console.log(this.inputNom.current.value);
   };
-
-  handleClick_2() {
-    console.log("Click_2");
-  }
-
-  handleClick_3 = id => {
-    console.log("Click_3 id = " + id);
-  };
-
-  handleClick_4(id) {
-    console.log("Click_4 id = " + id);
-  }
 
   render() {
     return (
       <div className="App">
-        <h3>
-          Passer un gestionnaire d’événements (par exemple onClick) à un
-          composant.
-        </h3>
-        <p>Première méthode : </p>
-        <button onClick={this.handleClick_1}>Cliquez ici</button>
-        <p>Deuxième méthode : </p>
-        <button onClick={this.handleClick_2.bind(this)}>Cliquez ici</button>
-        <h3>
-          Passer un argument à un gestionnaire d’événements ou une fonction de
-          rappel.
-        </h3>
-        state id = {this.state.id} <br />
-        <p>Première méthode : </p>
-        <button onClick={() => this.handleClick_3(this.state.id)}>
-          Cliquez ici
-        </button>
-        <p>Deuxième méthode : </p>
-        <button onClick={this.handleClick_4.bind(this, this.state.id)}>
-          Cliquez ici
-        </button>
+        <h3>Gérer un formulaire avec react.</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            ref={this.inputNom}
+            type="text"
+            name="name"
+            placeholder="Entrez votre nom"
+          />
+          <input type="submit" value="Envoyer" />
+        </form>
       </div>
     );
   }
